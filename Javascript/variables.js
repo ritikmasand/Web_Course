c005d8409fa04e9e749d046b6f9c3927e59e6d5a
ritikmasand <ritikmasand10@gmail.com>
2023-09-26 22:14:46 +0530

    javscript variabled and functiond

---
+++ Javascript/variables.js
@@ -0,0 +1,56 @@
+// var a; // variable declaration
+// a=10;  // variable assignment
+
+// var
+// var a = 10; // shortcut for declaring and assigning altogther
+
+// console.log(a); // 10
+
+// {
+//   a = 20; // this is a block
+// }
+//    console.log(a);
+// let
+// let a = 10;
+
+// console.log(a); // 10
+
+// {
+//   let a = 20;
+//   console.log(a); // 20
+// }
+// console.log(a);  // 10
+
+// re-declare  ✅
+// re-assign   ✅
+
+// const
+
+// const a = 10;
+// console.log(a); // 10
+// {
+// //   a = 20;
+//   console.log(a); // undefined
+// }
+// console.log(a); //  10
+
+// re-assign   ❌
+// re-declare  ❌
+
+// var a = 10;
+// var b = 20;
+// var c = a+b;
+// console.log(c);
+
+// var c=40;
+// var d=80
+// var e = 40+80;
+// console.log(e);
+
+function add() {
+  console.log('ritik');
+}
+// add(10, 10);
+// add(20, 20);
+// add(100,100)
+add()
