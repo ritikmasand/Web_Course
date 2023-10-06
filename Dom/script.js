691d61e8dac8457d5d4ea9e924b6be1a28bb24a9
ritikmasand <ritikmasand10@gmail.com>
2023-10-06 22:10:31 +0530

    dom

---
+++ Dom/script.js
@@ -0,0 +1,9 @@
+// let divref = document.getElementsByClassName("demo");
+// console.log(divref);
+
+// divref[1].innerHTML = "ritik";
+
+// var node = document.getElementsByName("animal");
+// console.log(node);
+let divref = document.querySelectorAll(".demo");
+console.log(divref);
